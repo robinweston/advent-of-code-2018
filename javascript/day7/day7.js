10e94e756bd1a72d5b6c50a296843ed19c514e71
Robin Weston <robin.weston@bcgdv.com>
2018-12-24 23:57:43 +0000

    Completed day 7

--- javascript/day7/day7.js
+++ javascript/day7/day7.js
@@ -47,8 +47,62 @@ const part1 = () => {
   console.log("Part 1:", ranSteps.join(""));
 };
 
+const constructionTime = (char, baseSeconds) =>
+  char.charCodeAt(0) - "A".charCodeAt(0) + 1 + baseSeconds;
+
 const part2 = () => {
-  console.log("Part 2:", "XXX");
+  const input = readInput();
+  const deps = input.split(/\r\n|\n/).map(lineToDep);
+
+  // const totalElves = 2;
+  // const baseSeconds = 0;
+  const totalElves = 5;
+  const baseSeconds = 60;
+
+  let remainingSteps = buildSteps(deps);
+  const completedSteps = [];
+  const inProgressSteps = [];
+
+  let elapsedSeconds = 0;
+  const elves = [...Array(totalElves)].map(_ => []);
+
+  while (remainingSteps.length > 0) {
+
+    // elves complete work
+    for (var i = 0; i < elves.length; i++) {
+      if (elves[i].length > 1) {
+        elves[i] = _.drop(elves[i]).join("");
+        assignNewWork = false;
+      } else if (elves[i].length === 1) {
+        const completedStepId = elves[i][0];
+        elves[i] = [];
+        completedSteps.push(completedStepId);
+        remainingSteps = remainingSteps.filter(rs => rs.id !== completedStepId);
+      }
+    }
+
+    // elves pick up new work
+    for (var i = 0; i < elves.length; i++) {
+      if (elves[i].length === 0) {
+        const validStep = _.chain(remainingSteps)
+          .filter(rs => canStepBeRun(rs, completedSteps))
+          .filter(rs => !_.includes(inProgressSteps, rs.id))
+          .sortBy(s => s.id)
+          .first()
+          .value();
+
+        if (validStep) {
+          inProgressSteps.push(validStep.id);
+          elves[i] = _.repeat(validStep.id, constructionTime(validStep.id, baseSeconds));
+        }
+      }
+    }
+
+    elapsedSeconds++;
+  }
+
+  console.log(completedSteps.join(""));
+  console.log("Part 2:", elapsedSeconds - 1);
 };
 
 part1();
