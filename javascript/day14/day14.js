daaefb87d259c5e2a359a299b97eff1946c4f56b
Robin Weston <robin.weston@bcgdv.com>
2018-12-30 22:22:02 +0000

    Day 14 done

--- javascript/day14/day14.js
+++ javascript/day14/day14.js
@@ -33,6 +33,43 @@ const scoreRecipes = n => {
   console.log(`Score 10 recipes after first ${n}: ${score}`);
 };
 
+const scoreRecipes2 = n => {
+  let elf1Position = 0;
+  let elf2Position = 1;
+  const scores = [3, 7];
+
+  let i = 0;
+  while (i < 50000000) {
+    const elf1Score = scores[elf1Position];
+    const elf2Score = scores[elf2Position];
+    const newScore = elf1Score + elf2Score;
+
+    if (newScore < 10) {
+      scores.push(newScore);
+    } else {
+      scores.push(getDigit(newScore, 2));
+      scores.push(getDigit(newScore, 1));
+    }
+
+    elf1Position = (elf1Position + 1 + elf1Score) % scores.length;
+    elf2Position = (elf2Position + 1 + elf2Score) % scores.length;
+
+    i++;
+
+    if (i % 100000 === 0) {
+      console.log(`Checking scores at ${i}`);
+      const score = scores.join("");
+      const scorePosition = score.indexOf(n);
+      if (scorePosition > 0) {
+        console.log(
+          `Recipies to left of score sequence ${n}: ${scorePosition}`
+        );
+        return;
+      }
+    }
+  }
+};
+
 const part1 = () => {
   console.log("Part 1:");
   scoreRecipes(9);
@@ -42,7 +79,14 @@ const part1 = () => {
   scoreRecipes(513401);
 };
 
-const part2 = () => {};
+const part2 = () => {
+  console.log("Part 2:");
+  scoreRecipes2("51589");
+  scoreRecipes2("01245");
+  scoreRecipes2("92510");
+  scoreRecipes2("59414");
+  scoreRecipes2("513401");
+};
 
 part1();
 part2();
